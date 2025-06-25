2884ebb468ca8468ff25fe0202292ae40c5cc6c4
alunoibc <alunoibc@CETAM.LOCAL>
2025-06-25 15:25:27 -0400

    adicionando cometários

--- CETAM/Project aula 2.py
+++ CETAM/Project aula 2.py
@@ -33,14 +33,14 @@
 # n2 = int(input('digite o segundo valor'))
 # print('O primeiro número pe:', n1, "o segundo número é:", n2)  #exibe os valores digitados pelo usuário
 
-n1 = float (input('digite a primeira nota:'))
-n2 = float (input('digite a segunda nota:'))
-media = (n1 + n2) / 2
-print('A média do aluno(a)é:',media)
-if media >=6:
-    print('Aprovado')
-else:
-    print('Reprovado') 
+# n1 = float (input('digite a primeira nota:'))
+# n2 = float (input('digite a segunda nota:'))
+# media = (n1 + n2) / 2
+# print('A média do aluno(a)é:',media)
+# if media >=6:
+#     print('Aprovado')
+# else:
+#     print('Reprovado') 
 
 # print(5 + 3)    # Soma -> 8
 # print(10 - 2)   # Subtração -> 8
