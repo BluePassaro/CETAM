a8101496cff8e8bb5b4f4eeee048cdc179b23e67
alunoibc <alunoibc@CETAM.LOCAL>
2025-06-25 14:24:43 -0400

    Adiciona exercício da aula de ontem

---
+++ CETAM/Project aula 2.py
@@ -0,0 +1,36 @@
+# nome = "Ivson Freire" #Uma string
+# idade = 27 #um valor inteiro    
+# altura = 1.70 #um valor em ponto flutuante
+
+# print(f"A quantidade de letras do meu nome é: {len(nome)}")
+# print(nome.upper())
+# print(nome.lower())
+# print(nome.title())
+
+# print(type(nome)) #Ivson    
+# print(type(idade)) #27
+# print(type(altura)) #1.70
+
+# nota1= 8
+# nota2= 6
+# nota3= 8
+# nota4= 3
+# soma(nota1,nota2,nota3,nota4)
+# soma(soma/2)
+
+# n1 = float(input('digite um valor'))
+# n2 = float(input('digite outro valor'))
+# soma = n1 + n2
+# print(soma)
+
+# nome = input('Qual seu nome?')
+# print ('Meu nome é',(nome))
+
+# print('-='* 20)
+# print(' LER DOIS NÚMEROS')
+# print('-='* 20)
+# n1 = int(input('digite o primeiro valor'))
+# n2 = int(input('digite o segundo valor'))
+# print('O primeiro número pe:', n1, "o segundo número é:", n2)  #exibe os valores digitados pelo usuário
+
+5 == 87
